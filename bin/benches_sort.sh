4230ef4d96d09a2f87eb0353d209f9defeea4d0e
Mauro Lacy <mauro@lacy.com.es>
2021-01-30 15:57:53 +0100

    Add size / compression_ratio columns to benches csv files

--- bin/benches_sort.sh
+++ bin/benches_sort.sh
@@ -3,15 +3,26 @@
 BASE=`dirname $0`/..
 BENCH_RESULTS="$BASE/benchresults"
 
-grep -A1 '^compression' "$BENCH_RESULTS/benches.txt" | grep -v change | sed '/^compression.*pack.*$/{N;s/\n/ /}' | grep -v '^--$' | awk '{print $1","$5" "$6}' | tee "$BENCH_RESULTS/benches.csv"
+# Filter/organize fields: bench,time,size
+egrep '(^compression\b|\btime:|\bbytes\b|^Benchmarking\b)' "$BENCH_RESULTS/benches.txt" | grep -v '^uncompressed:' | sed 's/^Benchmarking\b.*//' | sed '/^compression\b/{N;N;s/\n/ /g}' | grep -v '^$' | awk '{print $1","$5" "$6","$10}' >"$BENCH_RESULTS/benches.csv"
+
+# Uncompressed file size
+USZ=`grep '^uncompressed:' benchresults/benches.txt | cut -d\  -f2`
 
 (
-echo "bench,time,ns"
+echo "bench,time,size,compression_ratio,ns"
 while read L
 do
-	V=`echo $L | cut -f2 -d, | sed 's/ ns$//;s/ us$/\*1000/;s/ ms$/*1000000/;s/ s$/*1000000000/' | bc -l`
-	echo "$L,$V"
-done <"$BENCH_RESULTS/benches.csv" | sort -t, -nk3
+	# Compute nanoseconds
+	NS=`echo $L | cut -f2 -d, | sed 's/ ns$//;s/ us$/\*1000/;s/ ms$/*1000000/;s/ s$/*1000000000/' | bc -l`
+
+  # Compute compression ratio
+	C=NA
+	SZ=`echo $L | cut -f3 -d,`
+	[ -n "$SZ" ] && C=`echo -e "scale=3\n$SZ/$USZ" | bc -l`
+
+	echo "$L,$C,$NS"
+done <"$BENCH_RESULTS/benches.csv" | sort -t, -nk5
 ) | tee "$BENCH_RESULTS/benches-2.csv"
 mv "$BENCH_RESULTS/benches-2.csv" "$BENCH_RESULTS/benches.csv"
 
